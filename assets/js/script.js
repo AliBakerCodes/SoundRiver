661aa448895ec94f56a96fe1cd269897a6827fd5
unknown <AliBakerCodes@gmail.com>
2022-04-14 03:27:40 -0400

    Major Playlist updates. Still includes static html for tracks. Soundcloud and Spotify embeds. Play button controls SC playback. Set even listener to Playlist element. Collapse button now works at card level

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,49 @@
-var expandBtn = document.querySelector(".expand");
-var collapsibleEl = document.querySelector(".collapsible");
+// -----------Main JavaScript. All Global JS goes here------------------------------
 
-expandBtn.addEventListener("click", function() {
-    if (collapsibleEl.style.display == "none") {
-        collapsibleEl.style.display = "block";
+// ------------Query Selectors----------------------
+var currentPlaylist = $('#currentPlaylist')
+
+//-------------Variables----------------------------
+const HIDE_CLASS = 'hide';
+
+// ------------Init Function------------------------
+function init(){
+    setEventListeners();
+}
+
+//-------------Functions----------------------------
+function collapse(ele){
+    if (ele.hasClass(HIDE_CLASS)) {
+        ele.removeClass(HIDE_CLASS);
     } else {
-        collapsibleEl.style.display = "none";
+        ele.addClass(HIDE_CLASS);
     }
-});
+};
+
+
+// ------------Event Listeners----------------------
+function setEventListeners() {
+    currentPlaylist.on("click", "button", function (event) {
+    console.log("click");
+      var target = $(event.currentTarget);
+      var index = target.index(this)
+      console.log(target);
+      console.log("Index: " + index)
+    //   if (target.hasClass("playTrack")) { //Play Button Click
+    //     console.log("Play Click");
+    //     console.log($(".playTrack").index(this));
+    //     // scPlayPause($(".playTrack").index(this))
+
+    //   };
+      if (target.hasClass("expand")) { //Play Button Click
+        console.log("Expand Button Click");
+        var ele=$(target).offsetParent().next()
+        console.log(ele);
+        collapse(ele);
+
+      }
+    }
+  )
+};
+
+init();
