336f3cb7557ba5c622434a8a2bbd88512c5cafcc
unknown <AliBakerCodes@gmail.com>
2022-04-15 17:20:52 -0400

    Some youtube test code

--- assets/js/yt.js
+++ assets/js/yt.js
@@ -1,4 +1,10 @@
 
+  var tag = document.createElement('script');
+  tag.id = 'iframe-demo';
+  tag.src = 'https://www.youtube.com/iframe_api';
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
 var player;
   function onYouTubeIframeAPIReady() {
     player = new YT.Player('yt-0', {
